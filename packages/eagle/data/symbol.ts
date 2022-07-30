9989fc2a904bedafc44f4ccd7aafae72a6a69f30
Zeffon <ZeffonWu@gmail.com>
2022-07-30 17:43:43 +0800

    feat: symbol intro

---
+++ packages/eagle/data/symbol.ts
@@ -0,0 +1,7 @@
+export const symbolTabItems = [
+  { value: 'symbols', label: '音标体系', aid: '414096645', page: 2 },
+  { value: 'sound', label: '发音过程', aid: '414096645', page: 4 },
+  { value: 'vowels', label: '元音三要素', aid: '414096645', page: 5 },
+  { value: 'vowelChart', label: '元音图', aid: '414096645', page: 6 },
+  { value: 'consonants', label: '辅音三要素', aid: '414096645', page: 27 }
+]
