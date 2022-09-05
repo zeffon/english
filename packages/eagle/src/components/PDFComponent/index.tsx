71f122effbcbb39006956a2dd1d1568587b91c47
Zeffon Wu <ZeffonWu@gmail.com>
2022-09-05 16:04:45 +0800

    fix: react-pdf use remove pdfjs

--- packages/eagle/src/components/PDFComponent/index.tsx
+++ packages/eagle/src/components/PDFComponent/index.tsx
@@ -1,8 +1,8 @@
 import React, { useRef, useState } from 'react'
-import { Document, Page, pdfjs } from 'react-pdf'
 import { useSize } from 'ahooks'
+import { Document, Page, pdfjs } from 'react-pdf/dist/esm/entry.webpack5'
 import styles from './styles.module.css'
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`
 
 const usePDF = (file: File) => {
   const [numPages, setNumPages] = useState(0)
@@ -46,10 +46,16 @@ const usePDF = (file: File) => {
     setPageNumber(pageNumber + 1)
   }
 
+  const options = {
+    cMapUrl: `https://unpkg.com/pdfjs-dist@${pdfjs.version}/cmaps/`,
+    cMapPacked: true,
+    standardFontDataUrl: 'standard_fonts/'
+  }
+
   const pdfRef = (
     <>
       <div ref={ref} className={styles.pdfContainer}>
-        <Document file={file} onLoadSuccess={onDocumentLoadSuccess}>
+        <Document file={file} onLoadSuccess={onDocumentLoadSuccess} options={options}>
           <Page pageNumber={pageNumber} width={size && size.width} />
         </Document>
       </div>
