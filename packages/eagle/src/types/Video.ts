930409f24a011ee2250802ae87908808a165fa4f
Zeffon Wu <ZeffonWu@gmail.com>
2022-12-23 17:51:48 +0800

    types: update VideoItemProps field

--- packages/eagle/src/types/Video.ts
+++ packages/eagle/src/types/Video.ts
@@ -1,16 +1,17 @@
 export interface VideoItemProps {
-  aid: number
-  author: string
+  id?: number // video id
+  aid?: number // video id
+  author?: string // author name
+  upper?: { name: string } // author name
   title: string
-  pic: string
-  cover: string
-  created?: number
-  ctime?: number
-  play?: number
-  length?: number
-  duration?: number
-  stat?: {
-    view: number
-  }
+  intro?: string
+  pic?: string // cover url
+  cover?: string // cover url
+  created?: number // create time
+  ctime?: number // create time
+  play?: number // play count
+  cnt_info: { play: number } // play count
+  duration?: number // duration
+  length?: string // duration
   [x: string]: any
 }
